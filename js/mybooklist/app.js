d9ef111bd2d4de5f09f18f3c0ed8d9ee250ad38c
Ilona Goldmane <songa17@inbox.lv>
2023-05-13 13:03:57 +0300

    feat: add form validation & alerts

--- js/mybooklist/app.js
+++ js/mybooklist/app.js
@@ -48,6 +48,17 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+
   static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
@@ -69,19 +80,31 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
 
-  // Instatiate book
-  const book = new Book(title, author, isbn);
-  //   console.log(book);
+  // Validate
+  if (title === '' || author === '' || isbn === '') {
+    // alert('Please fill in all fields');
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+    //   console.log(book);
 
-  // Add Book to UI
-  UI.addBookToList(book);
+    // Add Book to UI
+    UI.addBookToList(book);
 
-  // Clear fields
-  UI.clearFields();
+    // Show success message
+    UI.showAlert('Book Added', 'success');
+
+    // Clear fields
+    UI.clearFields();
+  }
 });
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
   //   console.log(e.target);
   UI.deleteBook(e.target);
+
+  // Show success message
+  UI.showAlert('Book Removed', 'success');
 });
